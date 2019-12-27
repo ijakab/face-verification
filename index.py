b46721299ae838fffd9412f2d498d09efdfc366b
Ivan Jakab <ivan.jakab@gauss.hr>
2019-12-27 18:22:09 +0100

    feat: getting face feature vectors and testing them

--- index.py
+++ index.py
@@ -3,20 +3,31 @@ from matplotlib import pyplot
 from PIL import Image
 from numpy import asarray
 from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-BASE_PATH = '/Users/gauss/Projects/faks/osrv/vggface'
-IMAGE_SIZE = (250, 250)
+BASE_PATH = '/Users/gauss/Projects/faks/osrv/vggface/'
+NUMBER_OF_IMAGES = 1
 
 
 def load_image(filename):
     loaded = pyplot.imread(BASE_PATH+filename)
     image = Image.fromarray(loaded)
-    image = image.resize(IMAGE_SIZE)
+    image = image.resize((250, 250))
     return asarray(image)
 
 
-img = load_image('/current/demo.jpg')
-print(img.shape)
-print(img)
+def get_feature_vectors(type):
+    images = []
+    for i in range(0, NUMBER_OF_IMAGES):
+        loaded = load_image(type + '/' + str(i) + '.jpg')
+        images.append(loaded)
+    images = asarray(images, 'float32')
+    processed = preprocess_input(images, version=2)
+    model = VGGFace(model='resnet50', include_top=False, input_shape=(250, 250, 3), pooling='avg')
+    return model.predict(processed)
+
+
+ftrs = get_feature_vectors('current')
+print(ftrs)
