a2458e8c2f98d911ef813a23f2508f228463e45b
Ivan Jakab <ivan.jakab@gauss.hr>
2019-12-27 18:06:03 +0100

    chore: project structure, image loading

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+from matplotlib import pyplot
+from PIL import Image
+from numpy import asarray
 from keras_vggface.vggface import VGGFace
 
-model = VGGFace(model='resnet50')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+BASE_PATH = '/Users/gauss/Projects/faks/osrv/vggface'
+IMAGE_SIZE = (250, 250)
+
+
+def load_image(filename):
+    loaded = pyplot.imread(BASE_PATH+filename)
+    image = Image.fromarray(loaded)
+    image = image.resize(IMAGE_SIZE)
+    return asarray(image)
+
+
+img = load_image('/current/demo.jpg')
+print(img.shape)
+print(img)
